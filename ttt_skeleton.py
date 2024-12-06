cfc0e75b2e721c1aa683b6857c9ec00a397354e9
Sophia Kolak <37517770+sophiakolak@users.noreply.github.com>
2024-12-06 14:22:29 -0500

    Create ttt_skeleton.py
    starter code for ttt project

---
+++ ttt_skeleton.py
@@ -0,0 +1,67 @@
+from tkinter import *
+from tkinter import messagebox 
+
+Player1 = 'X'
+stop_game = False
+
+
+'''
+This method should react when a certain square in the grid is pressed. 
+r=row, c=column. You should  
+1. update the GUI to display either an x or a y 
+2. update your state variable  
+3. call another method to check if the game has ended 
+'''
+def clicked(r,c):
+    global Player1
+    '''
+    Your code here 
+    '''
+    pass 
+
+'''
+This method should check whether or not the game has been won. 
+If win, produce a message box that says which player won. 
+If tie, produce a message box announcing a tie. 
+If neither, do nothing.
+'''
+def check_if_win():
+    global stop_game
+    '''
+    Your code here
+    '''
+    pass 
+
+# Design window
+#Creating the Canvas 
+root = Tk()
+# Title of the window             
+root.title("Your title here")  
+root.resizable(0,0)
+ 
+#Button
+b = [
+     [0,0,0],
+     [0,0,0],
+     [0,0,0]]
+ 
+#text for buttons
+states = [
+     [0,0,0],
+     [0,0,0],
+     [0,0,0]]
+ 
+for i in range(3):
+    for j in range(3): 
+
+        #creates a 2d matrix of buttons                                  
+        b[i][j] = Button(
+                        height = 4, width = 8, 
+                        font = ("Helvetica","20"), 
+                        command = lambda r = i, c = j : clicked(r,c))
+        
+        #positions them in a grid for the display
+        b[i][j].grid(row = i, column = j)
+
+#starts the game loop 
+mainloop()   
